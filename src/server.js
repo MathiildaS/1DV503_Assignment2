54f44102657e76641c8776b0e3b830948105b654
Mathilda Segerlund <ms228qs@student.lnu.se>
2026-01-05 00:14:34 +0100

    Add server.js

---
+++ src/server.js
@@ -0,0 +1,50 @@
+import express from 'express'
+import expressLayouts from 'express-ejs-layouts'
+import session from 'express-session'
+import logger from 'morgan'
+import { dirname, join } from 'node:path'
+import { fileURLToPath } from 'node:url'
+import { sessionConfig } from './config/sessions.js'
+import { router } from './routes/router.js'
+
+const expressApp = express()
+
+const rootDirectory = dirname(fileURLToPath(import.meta.url))
+const baseURL = process.env.BASE_URL || '/'
+
+expressApp.use(logger('dev'))
+expressApp.set('view engine', 'ejs')
+expressApp.set('views', join(rootDirectory, 'views'))
+expressApp.set('layout', join(rootDirectory, 'views', 'layouts', 'mainLayout'))
+expressApp.set('layout extractScripts', true)
+expressApp.set('layout extractStyles', true)
+expressApp.use(expressLayouts)
+
+expressApp.use(express.urlencoded({ extended: false }))
+expressApp.use(express.static(join(rootDirectory, '..', 'public')))
+
+if (process.env.NODE_ENV === 'production') {
+  expressApp.set('trust proxy', 1)
+}
+expressApp.use(session(sessionConfig))
+
+expressApp.use((req, res, next) => {
+  if (req.session.flash) {
+    res.locals.flash = req.session.flash
+    delete req.session.flash
+  }
+  res.locals.baseURL = baseURL
+  res.locals.session = req.session
+  next()
+})
+
+expressApp.use('/', router)
+
+expressApp.use((err, req, res, next) => {
+  console.error(err)
+  res.status(err.status || 500).render('errors/error', { error: err })
+})
+
+const server = expressApp.listen(process.env.PORT, () => {
+  console.log(`Server running at http://localhost:${server.address().port}`)
+})
