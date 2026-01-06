3bbd3a7accbfefc64803176e9d4003c2ec5546b0
Mathilda Segerlund <ms228qs@student.lnu.se>
2026-01-07 00:43:04 +0100

    Define the user router to render the sign up and log in form, post and handle log out

---
+++ src/routes/userRouter.js
@@ -0,0 +1,31 @@
+/**
+ * @file Defines the user router.
+ * @module userRouter
+ * @author Mathilda Segerlund <ms228qs@student.lnu.se>
+ */
+
+import express from 'express'
+import { UserController } from '../controllers/userController.js'
+
+// Export and create an Express router-instance.
+export const router = express.Router()
+
+const userController = new UserController()
+
+// Render the sign-up form.
+router.get('/signUp', userController.signUpForm)
+
+// Handle user registration.
+router.post('/signUp', userController.postSignUp)
+
+// Render the login form.
+router.get('/logIn', userController.logInForm)
+
+// Handle user login.
+router.post('/logIn', userController.postLogIn)
+
+// Render the logout-page.
+router.get('/logOut', userController.logOut)
+
+// Handle user logout. Only for logged in users.
+router.post('/logOut', userController.postLogOut)
