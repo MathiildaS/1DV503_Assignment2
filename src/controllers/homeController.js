3d354afc4de0b6c0c1f7f71749a7fa2330aa2ab3
Mathilda Segerlund <ms228qs@student.lnu.se>
2026-01-07 00:03:03 +0100

    Encapsulates a controller for the home page

---
+++ src/controllers/homeController.js
@@ -0,0 +1,26 @@
+/**
+ * @file Defines the HomeController class.
+ * @module homeController
+ * @author Mathilda Segerlund <ms228qs@student.lnu.se>
+ */
+
+/**
+ * Encapsulates a controller for the home page.
+ */
+export class HomeController {
+  /**
+   * Renders the home/index view and sends the rendered HTML as an HTTP response.
+   * Handles GET requests to '/'.
+   *
+   * @param {object} req - Express request object.
+   * @param {object} res - Express response object.
+   * @param {Function} next - Express next middleware function.
+   */
+  index (req, res, next) {
+    try {
+      res.render('home/index')
+    } catch (error) {
+      next(error)
+    }
+  }
+}
