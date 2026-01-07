b5c55d0b311fb00e87df6b828143023f7e540ea9
Mathilda Segerlund <ms228qs@student.lnu.se>
2026-01-07 11:42:01 +0100

    Define the book router and create bookController

---
+++ src/routes/booksRouter.js
@@ -0,0 +1,16 @@
+/**
+ * @file Defines the books router.
+ * @module booksRouter
+ * @author Mathilda Segerlund <ms228qs@student.lnu.se>
+ */
+
+import express from 'express'
+import { BooksController } from '../controllers/bookController.js'
+
+// Export and create an Express router-instance.
+export const router = express.Router()
+
+const booksController = new BooksController()
+
+// Render the books list.
+router.get('/', booksController.listBooks)
