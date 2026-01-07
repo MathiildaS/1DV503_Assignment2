4648212f6ff538ddde6035446f260af4f8db371c
Mathilda Segerlund <ms228qs@student.lnu.se>
2026-01-07 11:44:15 +0100

    Define sub router for books route

--- src/routes/router.js
+++ src/routes/router.js
@@ -6,6 +6,7 @@
 import express from 'express'
 import { router as homeRouter } from './homeRouter.js'
 import { router as userRouter } from './userRouter.js'
+import { router as booksRouter } from './booksRouter.js'
 
 // Export and create an Express router-instance.
 export const router = express.Router()
@@ -15,6 +16,7 @@ router.get('/favicon.ico', (req, res) => res.status(204).end())
 // Register the sub-router for home page and user routes
 router.use('/', homeRouter)
 router.use('/user', userRouter)
+router.use('/books', booksRouter)
 
 // Catch 404 if wrong routes.
 router.use((req, res, next) => {
